bb9439d2976698fce9e4da56748c9e04024016d5
armaneus <jwtaylor@cpp.edu>
2026-04-19 02:25:59 +0200

    Updated homework script

--- student_script.py
+++ student_script.py
@@ -1,6 +1,6 @@
 import ROOT
-h = ROOT.TH1F("h", "Homework Histogram", 100, 0, 10)
-for i in range(1000):
+h = ROOT.TH1F("h", "Homework Histogram", 1000, 0, 10)
+for i in range(5000):
 h.Fill(ROOT.gRandom.Gaus(5, 3))
 print("Entries=", h.GetEntries())
 EOF
